692f357686608179cbacc9957eaf40badc763e4b
Aljaz Potocnik <zajlap@gmail.com>
2022-02-02 17:28:30 +0100

    Model

--- classification.py
+++ classification.py
@@ -5,7 +5,7 @@ import keras
 from keras.models import Sequential
 from keras.layers import Dense, Conv2D , MaxPool2D , Flatten , Dropout 
 from keras.preprocessing.image import ImageDataGenerator
-#from keras.optimizers import Adam
+from tensorflow.keras.optimizers import Adam
 
 from sklearn.metrics import classification_report,confusion_matrix
 
@@ -91,3 +91,29 @@ model.add(Dense(128, activation="relu"))
 model.add(Dense(2, activation="softmax"))
 
 model.summary()
+
+opt = Adam(learning_rate=0.000001)
+model.compile(opt,loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True) , metrics = ['accuracy'])
+history = model.fit(x_train,y_train,epochs = 500 , validation_data = (x_val, y_val))
+
+
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs_range = range(500)
+
+plt.figure(figsize=(15, 15))
+plt.subplot(2, 2, 1)
+plt.plot(epochs_range, acc, label='Training Accuracy')
+plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.title('Training and Validation Accuracy')
+
+plt.subplot(2, 2, 2)
+plt.plot(epochs_range, loss, label='Training Loss')
+plt.plot(epochs_range, val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.title('Training and Validation Loss')
+plt.show()
